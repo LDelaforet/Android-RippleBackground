0a07a4a2f08a4feb7e792ac7823a70c240caba55
LDelaforet <gillkubrus@gmail.com>
2023-12-11 22:20:08 +0100

    Nettoyage de dépendences inutiles.

--- rippleBackground/build.gradle.kts
+++ rippleBackground/build.gradle.kts
@@ -48,11 +48,6 @@ android {
 }
 
 dependencies {
-    implementation("com.android.library:com.android.library.gradle.plugin:8.0.0")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    implementation("com.github.dcendents:android-maven-gradle-plugin:2.1")
-    implementation("androidx.appcompat:appcompat-resources:1.6.0")
     implementation("androidx.appcompat:appcompat:1.6.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
@@ -60,11 +55,7 @@ dependencies {
     implementation("androidx.navigation:navigation-fragment-ktx:2.5.3")
     implementation("androidx.navigation:navigation-ui-ktx:2.5.3")
     implementation("androidx.recyclerview:recyclerview:1.3.2")
-    implementation("com.github.squti:Android-Wave-Recorder:1.7.0")
     implementation("com.google.android.material:material:1.8.0")
-    implementation("com.google.code.gson:gson:2.10.1")
-    implementation("com.google.firebase:firebase-crashlytics-buildtools:2.9.9")
-    testImplementation("junit:junit:4.13.2")
     implementation("androidx.core:core-ktx:1.9.0")
 }
 
